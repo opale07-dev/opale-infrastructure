22c26a0448c305eda7ae71e7d717c9e1a8070ddc
Greg <mendix@metayer.ch>
2026-06-28 01:57:36 +0200

    update secrets

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,8 +3,3 @@ output "instance_ip" {
   value       = openstack_compute_instance_v2.opale_vault.access_ip_v4
   description = "L'IP publique de ton instance Opale Vault"
 }
-
-output "private_key" {
-  value     = openstack_compute_keypair_v2.opale_key.private_key
-  sensitive = true
-}
