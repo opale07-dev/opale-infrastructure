e0f9bbdf4602f7d9c57af9febce49886b006a3b4
Greg <mendix@metayer.ch>
2026-06-27 20:53:44 +0200

    s3 config

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,17 @@ terraform {
     bucket                      = "opale-core-tfstate"
     key                         = "prod/terraform.tfstate"
     region                      = "us-east-1"
+
+    endpoints {
+      s3 = "https://s3.pub1.infomaniak.cloud" 
+    }
+    
+    use_path_style              = true 
+    skip_requesting_account_id  = true
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
   }
 }
 
