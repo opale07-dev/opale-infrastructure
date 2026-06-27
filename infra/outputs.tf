8d0bcb53f4a908b86bc708b7d8a42652c4824ffa
Greg <mendix@metayer.ch>
2026-06-27 21:37:18 +0200

    rename vm

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,5 @@
 # Output pour récupérer l'IP facilement
 output "instance_ip" {
-  value       = openstack_compute_instance_v2.opale_core.access_ip_v4
-  description = "L'IP publique de ton instance Opale Core"
+  value       = openstack_compute_instance_v2.opale_vault.access_ip_v4
+  description = "L'IP publique de ton instance Opale Vault"
 }
