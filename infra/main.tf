d6327793ba6a4ea3a76e83cad768734b88ee2ed8
Greg <mendix@metayer.ch>
2026-06-27 21:00:44 +0200

    s3 config

--- infra/main.tf
+++ infra/main.tf
@@ -13,10 +13,6 @@ terraform {
     key                         = "prod/terraform.tfstate"
     region                      = "us-east-1"
 
-    endpoints {
-      s3 = "https://s3.pub1.infomaniak.cloud" 
-    }
-    
     use_path_style              = true 
     skip_requesting_account_id  = true
     skip_credentials_validation = true
